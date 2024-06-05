138a5c083179b819fbb5e91b75cc0900cf6be2a7
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2024-06-04 23:09:15 -0300

    botao de sair

--- SistemaHotel/src/telas/TelaLogin.java
+++ SistemaHotel/src/telas/TelaLogin.java
@@ -126,7 +126,7 @@ public class TelaLogin extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
