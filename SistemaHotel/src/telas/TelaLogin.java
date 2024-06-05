472067d0925f21881bdaab445256766c312d0382
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2024-06-04 21:21:04 -0300

    tobias

--- SistemaHotel/src/telas/TelaLogin.java
+++ SistemaHotel/src/telas/TelaLogin.java
@@ -55,6 +55,11 @@ public class TelaLogin extends javax.swing.JFrame {
         jButton1.setText("Entrar");
 
         jButton2.setText("Sair");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -101,6 +106,10 @@ public class TelaLogin extends javax.swing.JFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
