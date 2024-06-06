afb908795fad4562206e62bc6968530433c8c836
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2024-06-06 20:48:32 -0300

    banco de dados e tela de funcionarios

--- SistemaHotel/src/dao/FuncionariosDao.java
+++ SistemaHotel/src/dao/FuncionariosDao.java
@@ -127,6 +127,133 @@ public class FuncionariosDao implements DaoGenerica<Funcionarios>{
         }
     }
     
+    @Override
+    public ArrayList<Funcionarios> consultar() {
+        
+        ArrayList<Funcionarios> listaFuncionarios = new ArrayList<Funcionarios>();
+        String sql = "SELECT c.ID_Funcionario, c.CPF, c.Nome, c.Telefone, c.Usuario, s.Tipo_Genero, c.Tipo_User "+
+                     "FROM Funcionario as c "+
+                     "LEFT JOIN Genero AS s ON (s.ID_Genero = c.FK1_Sexo) "+  
+                     "ORDER BY c.ID_Funcionario ";
+        
+        try
+        {
+            if(this.conexao.conectar())
+            {
+                PreparedStatement sentenca = this.conexao.getConnection().prepareStatement(sql);
+                
+                //recebe o resultado da consulta
+                ResultSet resultadoSentenca = sentenca.executeQuery();
 
+                //percorrer cada linha do resultado
+                while(resultadoSentenca.next()) 
+                {
+                    //resgata o valor de cada linha, selecionando pelo nome de cada coluna da tabela Escola
+                    Funcionarios funcionario = new Funcionarios();
+                    funcionario.setID_funcionario(resultadoSentenca.getInt("ID_Funcionario"));
+                    funcionario.setCPF(resultadoSentenca.getInt("CPF"));
+                    funcionario.setNome(resultadoSentenca.getString("Nome"));
+                    funcionario.setTelefone(resultadoSentenca.getInt("Telefone"));
+                    funcionario.setUsuario(resultadoSentenca.getString("Usuario"));
+                    funcionario.setFK1_Sexo(resultadoSentenca.getInt("FK1_Sexo"));
+                    funcionario.setTipo_User(resultadoSentenca.getInt("Tipo_User"));
+                    listaFuncionarios.add(funcionario);
+                }
+
+                sentenca.close();
+                this.conexao.getConnection().close();
+            }
+            
+            return listaFuncionarios;
+        }
+        catch(SQLException ex)
+        {
+           throw new RuntimeException(ex);
+        }
+    }
+    public ArrayList<Funcionarios> consultar(String str) {
+        
+        ArrayList<Funcionarios> listaFuncionariosStr = new ArrayList<Funcionarios>();
+        String sql = "SELECT c.ID_Funcionario, c.CPF, c.Nome, c.Telefone, c.Usuario, s.Tipo_Genero, c.Tipo_User "+
+                     "FROM Funcionarios as c "+
+                     "LEFT JOIN Genero AS s ON (s.ID_Genero = c.FK1_Sexo) "+
+                     "WHERE ( UPPER(c.Nome like UPPER(?))) "+   
+                     "ORDER BY s.Tipo_Genero ";
+        
+        try
+        {
+            if(this.conexao.conectar())
+            {
+                PreparedStatement sentenca = this.conexao.getConnection().prepareStatement(sql);
+                
+                //recebe o resultado da consulta
+                sentenca.setString(1, "%"+str+"%");
+                ResultSet resultadoSentenca = sentenca.executeQuery();
+
+                //percorrer cada linha do resultado
+                while(resultadoSentenca.next()) 
+                {
+                    //resgata o valor de cada linha, selecionando pelo nome de cada coluna da tabela Escola
+                    Funcionarios funcionario = new Funcionarios();
+                    funcionario.setID_funcionario(resultadoSentenca.getInt("ID_Funcionario"));
+                    funcionario.setCPF(resultadoSentenca.getInt("CPF"));
+                    funcionario.setNome(resultadoSentenca.getString("Nome"));
+                    funcionario.setTelefone(resultadoSentenca.getInt("Telefone"));
+                    funcionario.setUsuario(resultadoSentenca.getString("Usuario"));
+                    funcionario.setFK1_Sexo(resultadoSentenca.getInt("FK1_Sexo"));
+                    funcionario.setTipo_User(resultadoSentenca.getInt("Tipo_User"));
+                    
+                    listaFuncionariosStr.add(funcionario);
+                }
+
+                sentenca.close();
+                this.conexao.getConnection().close();
+            }
+            
+            return listaFuncionariosStr;
+        }
+        catch(SQLException ex)
+        {
+           throw new RuntimeException(ex);
+        }
+    }
+    public ArrayList<Funcionarios> dashboard() {
+        
+        ArrayList<Funcionarios> ListarDashBoard = new ArrayList<Funcionarios>();
+//        String sql = "select count(idcad) as numcad, count(idcad)*2 as sumcad, (select count(idsexo)+100 from cadsexo) as numsexualidade from cadbasico;";
+        String sql = "select FLOOR(RAND()(10-5+1)*10) as numcad, FLOOR(RAND()(10-5+1)10) as sumcad, FLOOR(RAND()(10-5+1)*10) as numsexualidade";
+        
+        try
+        {
+            if(this.conexao.conectar())
+            {
+                PreparedStatement sentenca = this.conexao.getConnection().prepareStatement(sql);
+                
+                //recebe o resultado da consulta
+                 ResultSet resultadoSentenca = sentenca.executeQuery();
+
+                //percorrer cada linha do resultado
+                while(resultadoSentenca.next()) 
+                {
+                    //resgata o valor de cada linha, selecionando pelo nome de cada coluna da tabela Escola
+ //                   Cadastro cadastro = new Cadastro();
+ //                   cadastro.setTotalCadastros(resultadoSentenca.getInt("numcad"));
+ //                   cadastro.SetSomaCodigos(resultadoSentenca.getInt("sumcad"));
+ //                   cadastro.SetNumSexualidade(resultadoSentenca.getInt("numsexualidade"));
+                    
+ //                   ListarDashBoard.add(cadastro);
+                }
+
+                sentenca.close();
+                this.conexao.getConnection().close();
+            }
+            
+            return ListarDashBoard;
+        }
+        catch(SQLException ex)
+        {
+           throw new RuntimeException(ex);
+        }
+    }
     
 }
